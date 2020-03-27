2a4e3a450bfbbdd092b474ae28be950115429111
Scott Regenthal <scottregenthal@gmail.com>
2020-03-26 21:42:32 -0600

    Change ActionMailer config in development to raise delivery errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,10 +73,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: config.app_domain }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port: "587",
+    port: 587,
     enable_starttls_auto: true,
     user_name: '<%= ENV["DEVELOPMENT_EMAIL_USERNAME"] %>',
     password: '<%= ENV["DEVELOPMENT_EMAIL_PASSWORD"] %>',
