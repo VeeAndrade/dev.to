108fd27de3e0cac26533ad760f42028617d7cbd0
Scott Regenthal <scottregenthal@gmail.com>
2020-03-28 12:38:20 -0600

    Revise spec setup, removing article from array

--- spec/mailers/daily_mailer_spec.rb
+++ spec/mailers/daily_mailer_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe DailyMailer, type: :mailer do
     end
 
     it "works correctly" do
-      email = described_class.daily_email(user, [article])
+      email = described_class.daily_email(user, article)
 
       expect(email.subject).not_to be_nil
       expect(email.to).to eq([user.email])
@@ -19,13 +19,13 @@ RSpec.describe DailyMailer, type: :mailer do
     end
 
     it "includes the tracking pixel" do
-      email = described_class.daily_email(user, [article])
+      email = described_class.daily_email(user, article)
 
       expect(email.body).to include("open.gif")
     end
 
     it "includes UTM params" do
-      email = described_class.daily_email(user, [article])
+      email = described_class.daily_email(user, article)
 
       expect(email.body).to include(CGI.escape("utm_medium=email"))
       expect(email.body).to include(CGI.escape("utm_source=daily_mailer"))
