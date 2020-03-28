fd3ecc8518718a701d52c0458d12c31370a53af7
Scott Regenthal <scottregenthal@gmail.com>
2020-03-28 15:55:19 -0600

    Article object updates made to guarantee email delivery

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -3,19 +3,19 @@ class DailyMailer < ApplicationMailer
 
   def daily_email(user, article)
     @user = user
-    @article = article
+    @article = article.first(1)
     @unsubscribe = generate_unsubscribe_token(@user.id, :email_daily_dev)
-    mail(to: @user.email, subject: generate_title)
+    mail(to: @user.email, subject: "Your Daily Dose of DEV")
   end
 
-  private
-
-  def generate_title
-    adjusted_title(@article).to_s
-  end
-
-  def adjusted_title(article)
-    title = article.title.strip
-    "\"#{title}\"" unless title.start_with? '"'
-  end
+  # private
+  #
+  # def generate_title
+  #   adjusted_title(@article).to_s
+  # end
+  #
+  # def adjusted_title(article)
+  #   title = article.title.strip
+  #   "\"#{title}\"" unless title.start_with? '"'
+  # end
 end
